b3111e6b39e1ce3c5421332c245565fcd1637326
sohrab NOROUZZADEH <sohrab.norouzzadeh@gmail.com>
2021-10-26 15:49:04 +0200

    ajout TypeMessage et TypeMarketer

---
+++ src/DataFixtures/TypeMarketerFixtures.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\TypeMarketer;
+use App\Entity\TypeMessage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TypeMarketerFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $typeMarketers = ["SPA","Eleveur"];
+
+        foreach ($typeMarketers as $typeMarketer){
+            $tyMar = new TypeMarketer();
+            $tyMar->setName($typeMarketer);
+            $manager->persist($tyMar);
+        }
+        $manager->flush();
+    }
+}
---
+++ src/DataFixtures/TypeMessageFixtures.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\TypeMessage;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TypeMessageFixtures extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $typeMessages = ["Demande d'adoption","Demande d'information"];
+
+        foreach ($typeMessages as $typeMessage){
+            $tyMes = new TypeMessage();
+            $tyMes->setName($typeMessage);
+            $manager->persist($tyMes);
+        }
+        $manager->flush();
+    }
+}
