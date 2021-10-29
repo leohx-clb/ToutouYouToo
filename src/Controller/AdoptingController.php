4a47248c3625cf8a166c7a0c6621fa4bc0a8f10a
sohrab NOROUZZADEH <sohrab.norouzzadeh@gmail.com>
2021-10-29 11:24:06 +0200

    Je fais validation de form adoptaing et hasher le password

--- src/Controller/AdoptingController.php
+++ src/Controller/AdoptingController.php
@@ -9,6 +9,7 @@ use phpDocumentor\Reflection\Types\True_;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdoptingController extends AbstractController
@@ -16,7 +17,7 @@ class AdoptingController extends AbstractController
     /**
      * @Route("/adopting", name="adopting")
      */
-    public function index(Request $request, EntityManagerInterface $em): Response
+    public function index(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
     {
         $adopting = new Adopting();
         $form = $this->createForm(AdoptingType::class, $adopting, [
@@ -25,6 +26,8 @@ class AdoptingController extends AbstractController
 
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
+            $pwd = $hasher->hashPassword($adopting, $adopting->getPassword());
+            $adopting->setpassword($pwd);
             $em->persist($adopting);
             $em->flush();
 
