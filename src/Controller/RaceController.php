0ddd8598bc882e10e0cb196f66a7b564d3e180f5
sohrab NOROUZZADEH <sohrab.norouzzadeh@gmail.com>
2021-11-02 11:41:46 +0100

    Ajouté formulaire-race

---
+++ src/Controller/RaceController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Race;
+use App\Form\RaceType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class RaceController extends AbstractController
+{
+    /**
+     * @Route("/race", name="race")
+     */
+    public function index(Request $request, EntityManagerInterface $em): Response
+    {
+        $race = new Race();
+
+        $form = $this->createForm(RaceType::class, $race,[
+            'submit' => true
+        ]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($race);
+            $em->flush();
+
+            return $this->redirectToRoute('race');
+        }
+        return $this->render('race/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
