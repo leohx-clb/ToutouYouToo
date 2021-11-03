77b97dc7f95df6e5fac482c20d6b8af5981dc191
Leonard <leonard.maron@gmail.com>
2021-11-03 10:07:38 +0100

    ajout formulaire type marketer avec vue plus suppresion code mort dans le twig de race

---
+++ src/Form/TypeMarketerType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\TypeMarketer;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TypeMarketerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name',TextType::class)
+        ;
+        if ($options['submit'] === true){
+            $builder->add('submit',SubmitType::class);
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => TypeMarketer::class,
+            'submit' => false,
+        ]);
+        $resolver->setRequired([
+            'submit',
+        ]);
+    }
+}
