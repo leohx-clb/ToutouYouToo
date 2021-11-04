89e0195663f9dc66754436db2f2379b54d28b917
VinzzZ <vincent.beaune69@gmail.com>
2021-11-04 11:33:41 +0100

    ajout de liste de chien dans home avec quelques infos, ajout des routes dans le controlleur

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Repository\DogRepository;
 use App\Repository\MarketerRepository;
+use App\Repository\PictureRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,18 +12,31 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     private MarketerRepository $marketerRepository;
+    private DogRepository $dogRepository;
+    private PictureRepository $pictureRepository;
 
     /**
      * @Route("/", name="home")
+     * @param MarketerRepository $marketerRepository
+     * @param DogRepository $dogRepository
+     * @param PictureRepository $pictureRepository
+     * @return Response
      */
-    public function index(MarketerRepository $marketerRepository): Response
+    public function index(MarketerRepository $marketerRepository, DogRepository $dogRepository, PictureRepository $pictureRepository): Response
     {
         $this->marketerRepository = $marketerRepository;
         $marketers = $marketerRepository->findAll();
+        $this->dogRepository = $dogRepository;
+        $dogs = $dogRepository->findAll();
+        $this->pictureRepository = $pictureRepository;
+        $pictures = $pictureRepository->findAll();
 
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
-            'marketers' => $marketers
+            'marketers' => $marketers,
+            'dogs' => $dogs,
+            'pictures' => $pictures
+
         ]);
     }
 }
